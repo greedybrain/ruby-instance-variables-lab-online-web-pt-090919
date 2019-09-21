51accba1d244cd1da59b7e19b08e2197cb190379
Naya  Willis <greedybrain18@gmail.com>
2019-09-21 17:58:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog 
 
   def name=(dog_name) # setting
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
   
   def name # getting
@@ -12,4 +12,6 @@ end
 
 lassie = Dog.new 
 lassie.name = "Lassie"
+
+puts lassie.name
 
